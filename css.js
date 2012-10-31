d27997426ece089566c81e350f3cff54fe18aab0
Joe Nelson <cred+github@begriffs.com>
2012-10-30 22:51:44 -0500

    Passes JSLint

--- css.js
+++ css.js
@@ -1,114 +1,128 @@
-script = document.createElement("script");
-script.setAttribute("src", "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js");
-document.getElementsByTagName("head")[0].appendChild(script);
+/*jslint browser: true, indent: 2, nomen: true, plusplus: true */
+/*global _: true */
+/*global $: true */
 
-script = document.createElement("script");
-script.setAttribute("src", "//code.jquery.com/jquery.min.js");
-document.getElementsByTagName("head")[0].appendChild(script);
+(function () {
+  "use strict";
 
-function computedCss(dom) {
-  var ret   = {},
+  function repeated(f) {
+    return function (ary) {
+      if (ary.length === 0) { return []; }
+      var seed = ary.pop();
+      return _.reduce(ary, function (memo, obj) { return f(memo, obj); }, seed);
+    };
+  }
+
+  var script,
+    intersect = repeated(function (a, b) {
+      var x, c = {};
+      for (x in a) {
+        if (a.hasOwnProperty(x) && b.hasOwnProperty(x) && _.isEqual(a[x], b[x])) {
+          c[x] = a[x];
+        }
+      }
+      return c;
+    });
+
+  script = document.createElement("script");
+  script.setAttribute("src", "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js");
+  document.getElementsByTagName("head")[0].appendChild(script);
+
+  script = document.createElement("script");
+  script.setAttribute("src", "//code.jquery.com/jquery.min.js");
+  document.getElementsByTagName("head")[0].appendChild(script);
+
+  function computedCss(dom) {
+    var ret = {}, prop,
       style = window.getComputedStyle ? window.getComputedStyle(dom, null) : dom.currentStyle;
-  if(!style) {
-    return this.css();
-  }
-  for(var prop in style) {
-    ret[prop] = style.getPropertyValue(prop);
-  }
-  return ret;
-}
-
-function repeated(f) {
-  return function(ary) {
-    if(ary.length == 0) { return [] };
-    var seed = ary.pop();
-    return _.reduce(ary, function(memo, obj) { return f(memo, obj) }, seed);
-  }
-}
-
-var intersect = repeated(function(a,b){
-  c = {};
-  for(x in a) {
-    if(x in b && _.isEqual(a[x], b[x]) ) {
-      c[x] = a[x];
+    for (prop in style) {
+      if (style.hasOwnProperty(prop)) {
+        ret[prop] = style.getPropertyValue(prop);
+      }
     }
+    return ret;
   }
-  return c;
-});
 
-function difference(a, b) {
-  if(!a || !b) { return {}; }
-  c = {};
-  for(x in a) {
-    if(!(x in b)) {
-      c[x] = a[x];
+  function difference(a, b) {
+    var x, c = {};
+
+    if (!a || !b) { return {}; }
+    for (x in a) {
+      if (a.hasOwnProperty(x) && !b.hasOwnProperty(x)) {
+        c[x] = a[x];
+      }
     }
+    return c;
   }
-  return c;
-}
 
-function consolidate(snode) {
-  newkids = _.map(snode.children, consolidate);
-  common  = intersect(_.map(newkids, function(k) { return k.css } ));
-  for(i in newkids) {
-    newkids[i].css = difference(newkids[i].css, common);
+  function consolidate(snode) {
+    var newkids = _.map(snode.children, consolidate),
+      common = intersect(_.map(newkids, function (k) { return k.css; })),
+      i;
+    for (i = 0; i < newkids.length; i++) {
+      newkids[i].css = difference(newkids[i].css, common);
+    }
+    $.extend(snode.css, common);
+    return {
+      tag: snode.tag,
+      klass: snode.klass,
+      id: snode.id,
+      css: snode.css,
+      children: newkids
+    };
   }
-  jQuery.extend(snode.css, common);
-  return {
-    tag: snode.tag,
-    class: snode.class,
-    id: snode.id,
-    css: snode.css,
-    children: newkids
-  }
-}
 
-function shadowDom(elt) {
-  var kids = elt.children();
-  return {
-    tag: elt[0].tagName,
-    class: elt.attr('class'),
-    id: elt.attr('id'),
-    css: computedCss(elt[0]),
-    children: _.map(elt.children(), function(c){ return shadowDom($(c)) })
+  function shadowDom(elt) {
+    return {
+      tag: elt[0].tagName,
+      klass: elt.attr('class'),
+      id: elt.attr('id'),
+      css: computedCss(elt[0]),
+      children: _.map(elt.children(), function (c) { return shadowDom($(c)); })
+    };
   }
-}
 
-function renderStylesheet(snode, path) {
-  var result   = "",
+  function renderStylesheet(snode, path) {
+    var result = "",
       hasAttrs = false,
-      block    = path + " {\n";
-  for(attr in snode.css) {
-    block += "\t" + attr + ': ' + snode.css[attr] + ";\n";
-    hasAttrs = true;
-  }
-  block += "}\n\n";
-  if(hasAttrs) {
-    result += block;
-  }
+      block    = path + " {\n",
+      attr,
+      i,
+      c,
+      seen     = {},
+      classy   = function (snode) {
+        var x = snode.tag;
+        if (snode.id) {
+          x = "#" + snode.id;
+        } else if (snode.klass) {
+          x += "." + snode.klass.split(' ').join('.');
+        }
+        return x;
+      },
+      relative = function (snode, path) {
+        return snode.id ? classy(snode) : path + ' > ' + classy(snode);
+      };
 
-  var classy   = function(snode) {
-    var x = snode.tag;
-    if(snode.id) {
-      x = "#" + snode.id;
-    } else if(snode.class) {
-      x += "." + snode.class.split(' ').join('.');
+    for (attr in snode.css) {
+      if (snode.css.hasOwnProperty(attr)) {
+        block += "\t" + attr + ': ' + snode.css[attr] + ";\n";
+        hasAttrs = true;
+      }
     }
-    return x;
-  }
-  var relative = function(snode, path) {
-    return snode.id ? classy(snode) : path + ' > ' + classy(snode);
-  }
-
-  var seen = {};
-  for(i in snode.children) {
-    var c = relative(snode.children[i], path);
-    if(! (c in seen)) {
-      seen[c] = true;
-      result += renderStylesheet(snode.children[i], c);
+    block += "}\n\n";
+    if (hasAttrs) {
+      result += block;
     }
-  }
-  return result;
-}
 
-renderStylesheet(consolidate(shadowDom($('body'))), 'body');
+    for (i = 0; i < snode.children.length; i++) {
+      c = relative(snode.children[i], path);
+      if (!seen.hasOwnProperty(c)) {
+        seen[c] = true;
+        result += renderStylesheet(snode.children[i], c);
+      }
+    }
+    return result;
+  }
+
+  return renderStylesheet(consolidate(shadowDom($('body'))), 'body');
+}());
