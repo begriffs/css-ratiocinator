9a4501b2980c000afef5599022774cc688c56f82
Joe Nelson <cred+github@begriffs.com>
2012-11-25 19:42:59 -0600

    Inject jquery and underscore from local files

--- ratiocinate.js
+++ ratiocinate.js
@@ -5,29 +5,22 @@
 
   var system = require('system'),
     page = require('webpage').create(),
-    url = system.args[1],
-    style;
+    url = system.args[1];
 
   page.onConsoleMessage = function (msg, lineNum, sourceId) {
     console.log(msg);
   };
   page.open(url, function (status) {
-    page.includeJs("https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js",
-      function () {
-        page.includeJs("https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js",
-          function () {
-            if (!page.injectJs('lib/css.js')) {
-              console.log("Could not inject css.js");
-              phantom.exit();
-            }
-            style = page.evaluate(function () {
-              var styles = window.simplerStyle();
-              _.each(_.pairs(styles), function (pair) {
-                window.renderStyle(pair[0], pair[1]);
-              });
-            });
-            phantom.exit();
-          })
+    page.injectJs("lib/jquery-1.8.2.js");
+    page.injectJs("lib/underscore-1.4.2.js");
+    page.injectJs('lib/css.js');
+
+    page.evaluate(function () {
+      var styles = window.simplerStyle();
+      _.each(_.pairs(styles), function (pair) {
+        window.renderStyle(pair[0], pair[1]);
       });
+    });
+    phantom.exit();
   });
 }());
