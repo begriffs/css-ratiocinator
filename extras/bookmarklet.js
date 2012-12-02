a3af6d2e5631b14f93caeea789941ef4d69ded53
Matt Gauger <matt.gauger@gmail.com>
2012-12-02 10:32:08 -0600

    Clean up

--- extras/bookmarklet.js
+++ extras/bookmarklet.js
@@ -1,4 +1,4 @@
-var el = document.createElement('div');
+window.msgs = document.createElement('div');
 var b = document.getElementsByTagName('body')[0];
 var otherLib = false;
 var msg = '';
@@ -7,39 +7,41 @@ var jQueryUri = 'http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.2/jquery.min.j
 var underscoreUri = 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js';
 var ratioUri = 'https://raw.github.com/begriffs/css-ratiocinator/master/lib/css.js';
 
-var getScript = function(url) {
-  var script = document.createElement('script');
+var getScript = function(url, success) {
+  script = document.createElement('script');
   script.src = url;
-  var head = document.getElementsByTagName('head')[0],
+  head = document.getElementsByTagName('head')[0];
   done = false;
+
   script.onload = script.onreadystatechange = function () {
-    if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+    if (!done && (!this.readyState || this.readyState == 'complete')) {
       done = true;
       script.onload = script.onreadystatechange = null;
       head.removeChild(script);
     }
   };
   head.appendChild(script);
-}
+  console.log(script);
+};
 
 var showMsg = function() {
-  el.innerHTML = msg;
-  b.appendChild(el);
+  msgs.innerHTML = msg;
+  b.appendChild(msgs);
   window.setTimeout(function () {
-      b.removeChild(el);
+      b.removeChild(msgs);
   }, 2500);
-}
+};
 
-el.style.position='fixed';
-el.style.width='320px';
-el.style.marginLeft='-110px';
-el.style.top='0';
-el.style.left='50%';
-el.style.padding='5px%2010px';
-el.style.zIndex=1001;
-el.style.font='16px Helvetica,arial,serif';
-el.style.color='#222';
-el.style.backgroundColor = '#D1EEEE';
+msgs.style.position='fixed';
+msgs.style.width='320px';
+msgs.style.marginLeft='-110px';
+msgs.style.top='0';
+msgs.style.left='50%';
+msgs.style.padding='5px%2010px';
+msgs.style.zIndex=1001;
+msgs.style.font='16px Helvetica,arial,serif';
+msgs.style.color='#222';
+msgs.style.backgroundColor = '#D1EEEE';
 
 if (typeof $ == 'function') {
   otherlib = true;
@@ -47,21 +49,18 @@ if (typeof $ == 'function') {
 
 if (typeof jQuery == 'undefined') {
   getScript(jQueryUri);
-  msg += '<p>This page is now jQuerified with v' + jQuery.fn.jquery + '</p>';
 } else {
-  msg = '<p>This page already using jQuery</p>';
+  msg = '<p>This page already using jQuery v' + jQuery.fn.jquery + '</p>';
 };
 
 if (typeof _ == 'undefined') {
   getScript(underscoreUri);
-  msg += '<p>This page is now Underscored with v' + _.VERSION + '</p>';
 } else {
-  msg += '<p>This page already using Underscore</p>';
+  msg += '<p>This page already using Underscore v' + _.VERSION + '</p>';
 }
 
 if (typeof simplerStyle == 'undefined') {
   getScript(ratioUri);
-  msg += '<p>Loaded Ratiocinator. Reticulating splines...</p>';
 } else {
   msg += '<p>This page already has Ratiocinator loaded</p>';
 }
