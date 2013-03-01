7a9d69613e94c29bafc66c6cf8d9e7826200c727
Josh Hoff <randomenduser@gmail.com>
2013-03-01 15:30:24 -0600

    be quiet by default
    don't output debugging information by default, that way the output of
    this tool can be piped around without stripping out extra information
    first.
    
    to do this I had to add the ability to process arguments.  to do THAT I
    added underscore to ratiocinate.js, so the 'require' line will need to
    be upgraded when underscore is updated/moved.  There might be a more
    desirable way to do this.
    
    as a final note, the verbose option is brittle because `verbose` needs
    to be considered when logging information out (are we logging output or
    messages?).  I was going to fix this by unifying logging under a
    'lib/log.js' file, or by overriding `console.log`, but this change
    seemed to be the smallest, so I opted for it instead :)

--- ratiocinate.js
+++ ratiocinate.js
@@ -3,30 +3,50 @@
 (function () {
   "use strict";
 
-  var system = require('system'),
-    page = require('webpage').create(),
-    fs = require('fs'),
-    url = system.args[1],
-    fonts;
+  var url, fonts,
+    _              = require('./vendor/underscore-1.4.2.js'),
+    args           = require('system').args.slice(1),
+    fs             = require('fs'),
+    page           = require('webpage').create(),
+    verbose        = false,
+    isOptionOrFlag = function(item) {
+      return item.length > 0 && item[0] === '-';
+    };
+
+  // parse arguments {{{
+
+  var optionsAndFlags = _.filter(args, isOptionOrFlag);
+  args                = _.reject(args, isOptionOrFlag);
+
+  verbose = _.contains(optionsAndFlags, '-v') || _.contains(optionsAndFlags, '--verbose');
+
+  if (args.length < 1 && verbose) {
+    console.log("No URL specified, please pass the name of a URL or file you'd like analysed");
+  } else {
+    url = args[0];
+  }
 
   // assume http if no protocol is specified
   // and we're not looking at a local file
   if (!url.match(/:\/\//)) {
     if (!fs.exists(url)) {
-      console.log('Missing protocol, assuming http');
       url = 'http://' + url;
-    } else {
+      verbose && console.log('Missing protocol, assuming http');
+    } else if(verbose) {
       console.log('"' + url + '" exists locally, using that.');
       console.log('Prepend a protocol (e.g. http:// or https://) to override this behavior');
     }
   }
 
+  // }}} parse arguments
+
   page.onConsoleMessage = function (msg) {
     console.log(msg);
   };
+
   page.open(url, function (status) {
     if (status !== 'success') {
-      console.log('Failed to load "' + url + '"');
+      verbose && console.log('Failed to load "' + url + '"');
     } else {
       page.injectJs("vendor/jquery-1.8.2.js");
       page.injectJs("vendor/underscore-1.4.2.js");
